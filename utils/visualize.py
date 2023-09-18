eec44a125baa7801142a57700f6c04d757b89f71
91097luke <lukecamilleri97@gmail.com>
2023-09-18 09:47:31 +0000

    + visualization script

---
+++ utils/visualize.py
@@ -0,0 +1,92 @@
+import random
+import time
+random.seed(time.time())
+
+import matplotlib.pyplot as plt
+import numpy as np
+import buteo as beo
+
+from utils.data_protocol import protocol_fewshot
+
+def render_s2_as_rgb(arr, channel_first=False):
+    # If there are nodata values, lets cast them to zero.
+    if np.ma.isMaskedArray(arr):
+        arr = np.ma.getdata(arr.filled(0))
+
+    if channel_first:
+        arr = beo.channel_first_to_last(arr)
+    # Select only Blue, green, and red. Then invert the order to have R-G-B
+    rgb_slice = arr[:, :, 0:3][:, :, ::-1]
+
+    # Clip the data to the quantiles, so the RGB render is not stretched to outliers,
+    # Which produces dark images.
+    rgb_slice = np.clip(
+        rgb_slice,
+        np.quantile(rgb_slice, 0.02),
+        np.quantile(rgb_slice, 0.98),
+    )
+
+    # The current slice is uint16, but we want an uint8 RGB render.
+    # We normalise the layer by dividing with the maximum value in the image.
+    # Then we multiply it by 255 (the max of uint8) to be in the normal RGB range.
+    rgb_slice = (rgb_slice / rgb_slice.max()) * 255.0
+
+    # We then round to the nearest integer and cast it to uint8.
+    rgb_slice = np.rint(rgb_slice).astype(np.uint8)
+
+    return rgb_slice
+
+
+def visualise(x, y, y_pred=None, images=5, channel_first=False, vmin=0, vmax=1, save_path=None):
+    rows = images
+    if y_pred is None:
+        columns = 2
+    else:
+        columns = 3
+    i = 0
+    fig = plt.figure(figsize=(10 * columns, 10 * rows))
+
+    indexes = random.sample(range(0, x.shape[0]), images)
+    for idx in indexes:
+        arr = x[idx]
+        rgb_image = render_s2_as_rgb(arr, channel_first)
+
+        i = i + 1
+        fig.add_subplot(rows, columns, i)
+        plt.imshow(rgb_image)
+        plt.axis('on')
+        plt.grid()
+
+        i = i + 1
+        fig.add_subplot(rows, columns, i)
+        plt.imshow(y[idx], vmin=vmin, vmax=vmax, cmap='magma')
+        plt.axis('on')
+        plt.grid()
+
+        if y_pred is not None:
+            i = i + 1
+            fig.add_subplot(rows, columns, i)
+            plt.imshow(y_pred[idx], vmin=vmin, vmax=vmax, cmap='magma')
+            plt.axis('on')
+            plt.grid()
+
+    fig.tight_layout()
+
+    del x
+    del y
+    del y_pred
+
+    if save_path is not None:
+        plt.savefig(save_path)
+    plt.close()
+
+def main():
+    x_train, y_train, x_val, y_val = protocol_fewshot('/phileo_data/downstream/downstream_dataset_patches_np/',
+                                                      dst='/phileo_data/downstream/downstream_datasets_nshot/',
+                                                      n=1,
+                                                      y='roads')
+
+    visualise(x=x_train, y=y_train, y_pred=None, images=8, channel_first=False, vmin=0, vmax=1, save_path='/home/lcamilleri/git_repos/phileo-testbed/test/test.png')
+
+if __name__ == '__main__':
+    main()
